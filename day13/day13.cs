e01df26ac46fa8cc2db4aa9afa779ab60c6034a9
bmartinez <bmartinez@inetsat.com>
2023-12-14 00:42:33 -0300

    13 part2

--- day13/day13.cs
+++ day13/day13.cs
@@ -4,11 +4,12 @@ using Xunit;
 
 public class day13
 {
-    long Line(string pattern, int rows, int cols, int stride_rows, int stride_cols)
+    long Line(string pattern, int rows, int cols, int stride_rows, int stride_cols, int smudges)
     {
         long result = 0;
         for (int row = 1; row < rows; ++row)
         {
+            int smudges1 = smudges;
             for (int curr = 0; curr < row && curr < rows - row; ++curr)
             {
                 for (int col = 0; col < cols; ++col)
@@ -16,41 +17,60 @@ public class day13
                     if (pattern[(row - curr - 1) * stride_rows + col * stride_cols] !=
                         pattern[(row + curr) * stride_rows + col * stride_cols])
                     {
-                        goto next_row;
+                        if (smudges1 > 0)
+                        {
+                            smudges1 -= 1;
+                        }
+                        else
+                        {
+                            goto next_row;
+                        }
                     }
                 }
             }
+            if (smudges1 > 0)
+                goto next_row;
             result += row;
         next_row: { }
         }
         return result;
     }
-    long Line(string pattern)
+    long Line(string pattern, int smudges)
     {
         int cols = pattern.IndexOf('\n');
         int rows = pattern.Length / (cols + "\n".Length);
-        long line_rows = Line(pattern, rows, cols, cols + "\n".Length, 1);
-        long line_cols = Line(pattern, cols, rows, 1, cols + "\n".Length);
+        long line_rows = Line(pattern, rows, cols, cols + "\n".Length, 1, smudges);
+        long line_cols = Line(pattern, cols, rows, 1, cols + "\n".Length, smudges);
         return 100 * line_rows + line_cols;
     }
-    long LinesFile(string filename)
+    long LinesFile(string filename, int smudges = 0)
     {
         return
             File.ReadAllText(filename)
             .Split("\n\n")
             .Select(x => x + "\n")
-            .Select(Line)
+            .Select(x => Line(x, smudges))
             .Sum();
     }
 
     [Fact]
-    public void Example()
+    public void part1_Example()
     {
         Assert.Equal(405, LinesFile("example.txt"));
     }
     [Fact]
-    public void Input()
+    public void part2_Example()
     {
-        Assert.Equal(33122, LinesFile("input.txt")); //16784 low
+        Assert.Equal(400, LinesFile("example.txt", smudges: 1));
+    }
+    [Fact]
+    public void part1_Input()
+    {
+        Assert.Equal(33122, LinesFile("input.txt"));
+    }
+    [Fact]
+    public void part2_Input()
+    {
+        Assert.Equal(32312, LinesFile("input.txt", smudges: 1));
     }
 }

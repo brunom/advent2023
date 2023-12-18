2be49ac58da2bde45b1903b11fc880e5c7ea24e1
Bruno <bruno.uy@gmail.com>
2023-12-18 09:59:41 -0300

    12 ArrangementsTable wip

--- day12/day12.cs
+++ day12/day12.cs
@@ -1,11 +1,16 @@
-using System;
 using System.Collections.Immutable;
-using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
-using System.Runtime.CompilerServices;
 using Xunit;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
+Console.WriteLine(
+    //new[] { ".???#??.?##?#??? 1,1,7" }
+    //new[] { ".???#?? 1,1" }
+    new[] { "?.#.? 1,1" }
+    //new[] { ".??...? 2,1" }
+    .Select(day12.Parse)
+    .Select(x => (rec: day12.ArrangementsRec(x), tab: day12.ArrangementsTable(x)))
+    .Select((p, i) => (p.rec, p.tab, i))
+    .Where(x => x.rec != x.tab)
+    .FirstOrDefault());
 struct EquatableMemory<T> : IEquatable<EquatableMemory<T>>
 {
     EquatableMemory(ReadOnlyMemory<T> m)
@@ -43,9 +48,7 @@ struct EquatableMemory<T> : IEquatable<EquatableMemory<T>>
 
 public class day12
 {
-    public static long Arrangements(string s) => Arrangements(s, 1);
-    public static long Arrangements5(string s) => Arrangements(s, 5);
-    static long Arrangements(string s, int unfolding)
+    public static (string springs, ImmutableArray<int> groups) Parse(string s)
     {
         if (s.Split() is not [string springs, string sgroups])
             throw new NotImplementedException();
@@ -55,34 +58,70 @@ public class day12
             .Split(',')
             .Select(s => int.Parse(s))
             .ToImmutableArray();
-
-        springs = string.Join('?', Enumerable.Repeat(springs, unfolding));
-        groups =
-            Enumerable.Repeat(groups, unfolding)
-            .SelectMany(x => x)
-            .ToImmutableArray();
-        Dictionary<(EquatableMemory<char> springs, EquatableMemory<int> groups), long> cache = new();
-        var result = Arrangements(springs.AsMemory(), groups.AsMemory(), cache);
-        return result;
+        return (springs, groups);
     }
-
-    static bool Any(ReadOnlySpan<char> s, char ch)
+    public static (string springs, ImmutableArray<int> groups) Unfold(int unfolding, (string springs, ImmutableArray<int> groups) sg)
     {
-        foreach (var ch2 in s)
-        {
-            if (ch2 == ch)
-                return true;
-        }
-        return false;
+        return (
+            string.Join('?', Enumerable.Repeat(sg.springs, unfolding)),
+            Enumerable.Repeat(sg.groups, unfolding)
+                .SelectMany(x => x)
+                .ToImmutableArray());
     }
-    static long Arrangements(
+    public static long ArrangementsTable((
+        string springs,
+        ImmutableArray<int> groups) sg)
+    {
+        var arr = new long[sg.springs.Length, sg.groups.Length];
+        int damaged = 0;
+        for (int s = 0; s < sg.springs.Length; s++)
+        {
+            if (sg.springs[s] == '.')
+                damaged = 0;
+            else
+                damaged++;
+
+            for (int g = 0; g < sg.groups.Length; g++)
+            {
+                long prev;
+                if (s == 0)
+                    prev = 0;
+                else if (sg.springs[s] == '#' && g + 1 == sg.groups.Length)
+                    prev = 0;
+                else
+                    prev = arr[s - 1, g];
+
+                long curr;
+                if (damaged < sg.groups[g])
+                    curr = 0;
+                else if (sg.groups[g] <= s && sg.springs[s - sg.groups[g]] == '#')
+                    curr = 0;
+                else if (s + 1 < sg.springs.Length && sg.springs[s + 1] == '#')
+                    curr = 0;
+                else if (g == 0)
+                    curr = 1;
+                else if (1 + sg.groups[g] <= s)
+                    curr = arr[s - sg.groups[g] - 1, g - 1];
+                else
+                    curr = 0;
+
+                arr[s, g] = prev + curr;
+            }
+        }
+        return arr[sg.springs.Length - 1, sg.groups.Length - 1];
+    }
+
+    public static long ArrangementsRec((
+        string springs,
+        ImmutableArray<int> groups) sg) => ArrangementsRecImpl(sg.springs.AsMemory(), sg.groups.AsMemory(), new());
+    static long ArrangementsRecImpl(
         ReadOnlyMemory<char> springs,
         ReadOnlyMemory<int> groups,
         Dictionary<(EquatableMemory<char> springs, EquatableMemory<int> groups), long> cache)
     {
         if (groups.Length == 0)
         {
-            if (Any(springs.Span, '#'))
+            if (MemoryExtensions.Contains(springs.Span, '#'))
             {
                 return 0;
             }
@@ -92,6 +131,7 @@ public class day12
             }
         }
 
+        // cache can also use ReadOnlyMemory, with lower hit rate.
         if (cache.TryGetValue((springs, groups), out long sum))
         {
             return sum;
@@ -100,7 +140,7 @@ public class day12
         int mid = groups.Length / 2;
         for (int i = 0; i + groups.Span[mid] <= springs.Length; ++i)
         {
-            if (Any(springs[i..(i + groups.Span[mid])].Span, '.'))
+            if (MemoryExtensions.Contains(springs[i..(i + groups.Span[mid])].Span, '.'))
                 continue;
 
             int prev;
@@ -127,8 +167,8 @@ public class day12
                     continue;
             }
 
-            var lhs = Arrangements(springs[..prev], groups[..mid], cache);
-            var rhs = Arrangements(springs[next..], groups[(mid + 1)..], cache);
+            var lhs = ArrangementsRecImpl(springs[..prev], groups[..mid], cache);
+            var rhs = ArrangementsRecImpl(springs[next..], groups[(mid + 1)..], cache);
 
             sum += lhs * rhs;
         }
@@ -137,14 +177,16 @@ public class day12
         return sum;
     }
 
-    [Fact] public void Test_Arrangements_Line1() => Assert.Equal(1, Arrangements("???.### 1,1,3"));
-    [Fact] public void Test_Arrangements_Line2() => Assert.Equal(4, Arrangements(".??..??...?##. 1,1,3"));
-    [Fact] public void Test_Arrangements_Line3() => Assert.Equal(1, Arrangements("?#?#?#?#?#?#?#? 1,3,1,6"));
-    [Fact] public void Test_Arrangements_Line4() => Assert.Equal(1, Arrangements("????.#...#... 4,1,1"));
-    [Fact] public void Test_Arrangements_Line5() => Assert.Equal(4, Arrangements("????.######..#####. 1,6,5"));
-    [Fact] public void Test_Arrangements_Line6() => Assert.Equal(10, Arrangements("?###???????? 3,2,1"));
-    [Fact] public void Test_part1_example() => Assert.Equal(21, File.ReadLines("example.txt").Sum(Arrangements));
-    [Fact] public void Test_part1_input() => Assert.Equal(7084, File.ReadLines("input.txt").Sum(Arrangements));
-    [Fact] public void Test_part2_example() => Assert.Equal(525152, File.ReadLines("example.txt").Sum(Arrangements5));
-    [Fact] public void Test_part2_input() => Assert.Equal(8414003326821, File.ReadLines("input.txt").Sum(Arrangements5));
+    static long Arrangements((string springs, ImmutableArray<int> groups) sg) => ArrangementsTable(sg);
+
+    [Fact] public void Test_Arrangements_Line1() => Assert.Equal(1, Arrangements(Parse("???.### 1,1,3")));
+    [Fact] public void Test_Arrangements_Line2() => Assert.Equal(4, Arrangements(Parse(".??..??...?##. 1,1,3")));
+    [Fact] public void Test_Arrangements_Line3() => Assert.Equal(1, Arrangements(Parse("?#?#?#?#?#?#?#? 1,3,1,6")));
+    [Fact] public void Test_Arrangements_Line4() => Assert.Equal(1, Arrangements(Parse("????.#...#... 4,1,1")));
+    [Fact] public void Test_Arrangements_Line5() => Assert.Equal(4, Arrangements(Parse("????.######..#####. 1,6,5")));
+    [Fact] public void Test_Arrangements_Line6() => Assert.Equal(10, Arrangements(Parse("?###???????? 3,2,1")));
+    [Fact] public void Test_part1_example() => Assert.Equal(21, File.ReadLines("example.txt").Sum(x => Arrangements(Parse(x))));
+    [Fact] public void Test_part1_input() => Assert.Equal(7084, File.ReadLines("input.txt").Sum(x => Arrangements(Parse(x))));
+    [Fact] public void Test_part2_example() => Assert.Equal(525152, File.ReadLines("example.txt").Sum(x => Arrangements(Unfold(5, Parse(x)))));
+    [Fact] public void Test_part2_input() => Assert.Equal(8414003326821, File.ReadLines("input.txt").Sum(x => Arrangements(Unfold(5, Parse(x)))));
 }

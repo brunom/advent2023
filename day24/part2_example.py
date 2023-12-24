2e246ea83d03a0121668abf0a5351768e9606a3d
Bruno <bruno.uy@gmail.com>
2023-12-24 17:08:32 -0300

    24 part2

---
+++ day24/part2_example.py
@@ -0,0 +1,3 @@
+from sympy import *
+q, w, e, r, t, y, a, b, c = symbols('q, w, e, r, t, y, a, b, c', real=True)
+nonlinsolve ([19 -2*a - q - r*a,13 +1*a - w - t*a,30 -2*a - e - y*a,18 -1*b - q - r*b,19 -1*b - w - t*b,22 -2*b - e - y*b,20 -2*c - q - r*c,25 -2*c - w - t*c,34 -4*c - e - y*c], [q,w,e,r,t,y,a,b,c])
